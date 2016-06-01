c9e995a7626d082aa29490b1b8837a59effbd980
lushanghaha <lushanghaha@hotmail.com>
2016-06-02 04:50:41 +0800

    Fix feed activity prototype

--- app/src/main/java/maclab/everywear/Feed.java
+++ app/src/main/java/maclab/everywear/Feed.java
@@ -2,12 +2,42 @@ package maclab.everywear;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Feed extends AppCompatActivity {
+    private ListView listView;
+    private FeedListAdapter listAdapter;
+    private ArrayList<FeedItem> feedItems = new ArrayList<>(5);
+    private FeedItem feedItem;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        feedItem = new FeedItem(1, "John", "wear", "status", "pic", "time", "url");
+        feedItems.add(feedItem);
+        feedItem = new FeedItem(2, "Mary", "wear", "status", "pic", "time", "url");
+        feedItems.add(feedItem);
+        feedItem = new FeedItem(3, "Jim", "wear", "status", "pic", "time", "url");
+        feedItems.add(feedItem);
+        feedItem = new FeedItem(4, "Jenny", "wear", "status", "pic", "time", "url");
+        feedItems.add(feedItem);
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+        listView = (ListView)findViewById(R.id.listview_feed);
+        listAdapter = new FeedListAdapter(this, feedItems);
+        listView.setAdapter(listAdapter);
+        /*
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> patent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "You choose " + feedItems.get(position).getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        */
     }
 }
--- app/src/main/java/maclab/everywear/MainActivity.java
+++ app/src/main/java/maclab/everywear/MainActivity.java
@@ -1,13 +1,31 @@
 package maclab.everywear;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_feed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_feed = (Button)findViewById(R.id.btn_feed);
+        btn_feed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, Feed.class);
+                Bundle bundle = new Bundle();
+                intent.putExtras(bundle); // empty bundle
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
