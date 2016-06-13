dc79c450af680661a14daf56cfc3f4b6bff818b4
zeroplusone <a711186@gmail.com>
2016-06-14 05:33:12 +0800

    Add FB login.
    Get user's id, name and picture.

--- app/src/main/java/maclab/everywear/MainActivity.java
+++ app/src/main/java/maclab/everywear/MainActivity.java
@@ -1,21 +1,44 @@
 package maclab.everywear;
 
 import android.content.Intent;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_feed;
     private Button btn_camera;
+    private Button btn_login;
+
+    private CallbackManager callbackManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // initial facebook sdk. put before the super.onCreate().
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        Log.d("FB", "start");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         btn_feed = (Button)findViewById(R.id.btn_feed);
         btn_feed.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
         btn_camera = (Button)findViewById(R.id.btn_camera);
         btn_camera.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,7 +63,78 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // FB login
+        btn_login = (Button) findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                onFblogin();
+            }
+        });
+
     }
+
+    // Private method to handle Facebook login and callback
+    private void onFblogin()
+    {
+        callbackManager = CallbackManager.Factory.create();
+
+        // Set permissions
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("email","user_photos","public_profile"));
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+
+                        Log.d("FB", "Success");
+
+                        GraphRequest request = GraphRequest.newMeRequest(
+                                loginResult.getAccessToken(),
+                                new GraphRequest.GraphJSONObjectCallback() {
+                                    @Override
+                                    public void onCompleted(JSONObject object, GraphResponse response) {
+                                        Log.v("LoginActivity", response.toString());
+
+                                        // Application code
+                                        try {
+                                            Log.d("FB",object.getString("id"));
+                                            Log.d("FB",object.getString("name"));
+                                            Log.d("FB",object.getString("picture"));
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+
+                                    }
+                                });
+                        Bundle parameters = new Bundle();
+                        parameters.putString("fields", "id,name,picture");
+                        request.setParameters(parameters);
+                        request.executeAsync();
+
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Log.d("FB","On cancel");
+                    }
+
+                    @Override
+                    public void onError(FacebookException error) {
+                        Log.d("FB",error.toString());
+                    }
+                });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+
 
 
 }
