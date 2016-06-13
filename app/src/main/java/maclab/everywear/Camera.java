bf2b53b0a7a7c0581e4c2948e0c4d8eea37b14c4
zeroplusone <a711186@gmail.com>
2016-06-13 15:06:27 +0800

    Get the full size picture from camera.

--- app/src/main/java/maclab/everywear/Camera.java
+++ app/src/main/java/maclab/everywear/Camera.java
@@ -2,7 +2,9 @@ package maclab.everywear;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -20,21 +22,22 @@ import java.util.Date;
 public class Camera extends AppCompatActivity {
 
     private final int REQUEST_CODE_CAMERA = 1;
-    private ImageView imageView;
+    private ImageView mImageView;
     private Button btn_post;
     private Button btn_cancel;
+
+    private String mCurrentPhotoPath;
+    private static final String JPEG_FILE_PREFIX = "IMG_";
+    private static final String JPEG_FILE_SUFFIX = ".jpg";
+    private AlbumStorageDirFactory mAlbumStorageDirFactory = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
-        imageView = (ImageView) findViewById(R.id.imageView);
-
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, REQUEST_CODE_CAMERA);
-
-        btn_post = (Button)findViewById(R.id.btn_post);
+        mImageView = (ImageView) findViewById(R.id.imageView);
+        btn_post = (Button) findViewById(R.id.btn_post);
         btn_post.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,29 +48,198 @@ public class Camera extends AppCompatActivity {
             }
         });
 
-        btn_cancel = (Button)findViewById(R.id.btn_cancel);
+        btn_cancel = (Button) findViewById(R.id.btn_cancel);
         btn_cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO) {
+            mAlbumStorageDirFactory = new FroyoAlbumDirFactory();
+        } else {
+            mAlbumStorageDirFactory = new BaseAlbumDirFactory();
+        }
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
+            String[] perms = {"android.permission.WRITE_EXTERNAL_STORAGE"};
+            int permsRequestCode = 200;
+            requestPermissions(perms, permsRequestCode);
+        } else {
+            dispatchTakePictureIntent();
+        }
     }
 
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
+    public void onRequestPermissionsResult(int permsRequestCode, String[] permissions, int[] grantResults) {
+
+        switch (permsRequestCode) {
+
+            case 200:
+
+                dispatchTakePictureIntent();
+                break;
+
+        }
+
+    }
+
+    private void dispatchTakePictureIntent() {
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        File f = null;
+        try {
+            f = setUpPhotoFile();
+            mCurrentPhotoPath = f.getAbsolutePath();
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+        } catch (IOException e) {
+            e.printStackTrace();
+            f = null;
+            mCurrentPhotoPath = null;
+        }
+
+        startActivityForResult(takePictureIntent, REQUEST_CODE_CAMERA);
+    }
+
+    private File setUpPhotoFile() throws IOException {
+
+        File f = createImageFile();
+        mCurrentPhotoPath = f.getAbsolutePath();
+
+        return f;
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = JPEG_FILE_PREFIX + timeStamp + "_";
+        File albumF = getAlbumDir();
+        File imageF = File.createTempFile(imageFileName, JPEG_FILE_SUFFIX, albumF);
+        return imageF;
+    }
+
+
+    private File getAlbumDir() {
+        File storageDir = null;
+
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+
+
+            storageDir = mAlbumStorageDirFactory.getAlbumStorageDir("CameraSample");
+
+            if (storageDir != null) {
+                if (!storageDir.mkdirs()) {
+                    if (!storageDir.exists()) {
+                        Log.d("CameraSample", "failed to create directory");
+                        return null;
+                    }
+                }
+            }
+
+        } else {
+            Log.v(getString(R.string.app_name), "External storage is not mounted READ/WRITE.");
+        }
+
+        return storageDir;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK)
-        {
+        if (resultCode == RESULT_OK) {
             if (requestCode == REQUEST_CODE_CAMERA)   // camera
             {
-                Bitmap bitmap = (Bitmap) data.getExtras().getParcelable("data");    //取得bitmap縮圖
-                imageView.setImageBitmap(bitmap);
+//                Bitmap bitmap = (Bitmap) data.getExtras().getParcelable("data");    //取得bitmap縮圖
+//                imageView.setImageBitmap(bitmap);
+                if (mCurrentPhotoPath != null) {
+                    setPic();
+                    galleryAddPic();
+                    mCurrentPhotoPath = null;
+                }
             }
 
         }
     }
+
+    private void setPic() {
+
+		/* There isn't enough memory to open up more than a couple camera photos */
+        /* So pre-scale the target bitmap into which the file is decoded */
+
+		/* Get the size of the ImageView */
+        int targetW = mImageView.getWidth();
+        int targetH = mImageView.getHeight();
+
+		/* Get the size of the image */
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+		/* Figure out which way needs to be reduced less */
+        int scaleFactor = 1;
+        if ((targetW > 0) || (targetH > 0)) {
+            scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+        }
+
+		/* Set bitmap options to scale the image decode target */
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+		/* Decode the JPEG file into a Bitmap */
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+
+		/* Associate the Bitmap to the ImageView */
+        mImageView.setImageBitmap(bitmap);
+        mImageView.setVisibility(View.VISIBLE);
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent("android.intent.action.MEDIA_SCANNER_SCAN_FILE");
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+
+    abstract class AlbumStorageDirFactory {
+        public abstract File getAlbumStorageDir(String albumName);
+    }
+
+    public final class BaseAlbumDirFactory extends AlbumStorageDirFactory {
+
+        // Standard storage location for digital camera files
+        private static final String CAMERA_DIR = "/dcim/";
+
+        @Override
+        public File getAlbumStorageDir(String albumName) {
+            return new File(
+                    Environment.getExternalStorageDirectory()
+                            + CAMERA_DIR
+                            + albumName
+            );
+        }
+    }
+
+    public final class FroyoAlbumDirFactory extends AlbumStorageDirFactory {
+
+        @Override
+        public File getAlbumStorageDir(String albumName) {
+            // TODO Auto-generated method stub
+            return new File(
+                    Environment.getExternalStoragePublicDirectory(
+                            Environment.DIRECTORY_PICTURES
+                    ),
+                    albumName
+            );
+        }
+    }
+
 }
