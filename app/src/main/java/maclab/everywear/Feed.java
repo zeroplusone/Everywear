902c01a065a30ebad08f8dd0ac7cd0db844bffb8
zeroplusone <a711186@gmail.com>
2016-06-15 06:08:44 +0800

    Add tab to feed and connect tab_camera

--- app/src/main/java/maclab/everywear/Feed.java
+++ app/src/main/java/maclab/everywear/Feed.java
@@ -1,7 +1,11 @@
 package maclab.everywear;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -12,10 +16,29 @@ public class Feed extends AppCompatActivity {
     private ArrayList<FeedItem> feedItems = new ArrayList<>(5);
     private FeedItem feedItem;
 
+    private ImageButton tab_feed;
+    private ImageButton tab_camera;
+    private ImageButton tab_setting;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+
+        tab_feed =(ImageButton)findViewById(R.id.tab_feed);
+        tab_camera =(ImageButton)findViewById(R.id.tab_camera);
+        tab_setting =(ImageButton)findViewById(R.id.tab_setting);
+
+        tab_feed.setOnClickListener(feedOnClickListener);
+        tab_camera.setOnClickListener(cameraOnClickListener);
+        tab_setting.setOnClickListener(settingOnClickListener);
+
+        loadFeed();
+    }
+
+    private void loadFeed(){
         feedItem = new FeedItem(1, "John", "wear", "status", "pic", "time", "url");
         feedItems.add(feedItem);
         feedItem = new FeedItem(2, "Mary", "wear", "status", "pic", "time", "url");
@@ -26,8 +49,8 @@ public class Feed extends AppCompatActivity {
         feedItems.add(feedItem);
 
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_feed);
+
+
         listView = (ListView)findViewById(R.id.listview_feed);
         listAdapter = new FeedListAdapter(this, feedItems);
         listView.setAdapter(listAdapter);
@@ -40,4 +63,30 @@ public class Feed extends AppCompatActivity {
         });
         */
     }
+
+    private ImageButton.OnClickListener feedOnClickListener = new ImageButton.OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
+
+    private ImageButton.OnClickListener cameraOnClickListener = new ImageButton.OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+            Intent cameraIntent = new Intent();
+            cameraIntent.setClass(Feed.this, Camera.class);
+            startActivity(cameraIntent);
+        }
+    };
+
+    private ImageButton.OnClickListener settingOnClickListener = new ImageButton.OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
 }
